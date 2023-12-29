b4a631cbdfdaf2b239696bda81d0ab88aea0ed00
Olle <hugo.berggren@gmail.com>
2023-12-28 21:35:46 -0500

    Update fluidd.sh
    download latest fluidd
    remove zip as a last step.

--- userpatches/overlay/install_components/fluidd.sh
+++ userpatches/overlay/install_components/fluidd.sh
@@ -3,8 +3,9 @@
 install_fluidd(){
     echo "🍰 install Fluidd"
     cd /home/debian
-    wget https://github.com/fluidd-core/fluidd/releases/download/v1.24.1/fluidd.zip
+    wget https://github.com/fluidd-core/fluidd/releases/latest/download/fluidd.zip
     unzip fluidd.zip -d fluidd
     chown -R debian:debian fluidd
     cp /tmp/overlay/fluidd/fluidd.cfg /home/debian/printer_data/config
+    rm fluidd.zip
 }
