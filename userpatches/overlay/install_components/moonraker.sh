101c2585c13772bab63edf173494434b640347f3
Elias Bakken <elias@iagent.no>
2024-03-20 12:29:16 +0100

    Fix opp for gpu, add ssh as service

--- userpatches/overlay/install_components/moonraker.sh
+++ userpatches/overlay/install_components/moonraker.sh
@@ -5,9 +5,10 @@ install_moonraker(){
     echo "🍰 install Moonraker"
     cd /home/debian
     git clone https://github.com/Arksine/moonraker
+    # We add the ssh service here since the file is overwritten on the first boot.
+    echo 'ssh' >> /home/debian/moonraker/moonraker/assets/default_allowed_services
     chown -R debian:debian moonraker
     su -c "/home/debian/moonraker/scripts/install-moonraker.sh" debian
     su -c "/home/debian/moonraker/scripts/set-policykit-rules.sh" debian
     cp /tmp/overlay/moonraker/moonraker-"${UI}".conf /home/debian/printer_data/config/moonraker.conf
-    echo "ssh" >> /home/debian/printer_data/moonraker.asvc
 }
