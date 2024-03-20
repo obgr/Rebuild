9595cf139f05ed909a31a880f9a19c5560830459
Elias Bakken <elias@iagent.no>
2024-03-20 22:50:10 +0100

    Do not alter the git repo. It dirties it.

--- userpatches/overlay/install_components/moonraker.sh
+++ userpatches/overlay/install_components/moonraker.sh
@@ -5,10 +5,9 @@ install_moonraker(){
     echo "🍰 install Moonraker"
     cd /home/debian
     git clone https://github.com/Arksine/moonraker
-    # We add the ssh service here since the file is overwritten on the first boot.
-    echo 'ssh' >> /home/debian/moonraker/moonraker/assets/default_allowed_services
     chown -R debian:debian moonraker
     su -c "/home/debian/moonraker/scripts/install-moonraker.sh" debian
     su -c "/home/debian/moonraker/scripts/set-policykit-rules.sh" debian
     cp /tmp/overlay/moonraker/moonraker-"${UI}".conf /home/debian/printer_data/config/moonraker.conf
+    cp /tmp/overlay/moonraker/moonraker.asvc /home/debian/printer_data/
 }
